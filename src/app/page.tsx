23b39440eb01c811ebaf249198ada388fe0df7db
dagemdworku-dev <dagemdworku+dev@gmail.com>
2024-02-08 09:55:49 +0300

    Add joke functionality to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+'use client'
+
 import Sticker from "@/components/Sticker";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [joke, setJoke] = useState("");
+  const updateJoke = () => {
+    const joke = "Why don't scientists trust atoms? Because they make up everything!";
+
+    setJoke(joke);
+  }
   return (
     <div className="flex flex-col min-h-screen justify-between p-24">
       <header className="text-center">
@@ -9,9 +18,9 @@ export default function Home() {
       </header>
 
       <main className="flex flex-col items-center justify-center">
-        <Sticker text="Why don't scientists trust atoms? Because they make up everything!" />
-        <button className="mt-8 px-6 py-3 bg-blue-500 text-white rounded-full font-bold text-xl hover:bg-blue-700 transition-colors duration-300">
-          Another One
+        {joke && <Sticker text={joke} />}
+        <button onClick={updateJoke} className="mt-8 px-6 py-3 bg-blue-500 text-white rounded-full font-bold text-xl hover:bg-blue-700 transition-colors duration-300">
+          {joke ? 'Another One' : 'Click Me'}
         </button>
       </main>
 
@@ -19,6 +28,5 @@ export default function Home() {
         <p className="text-sm text-gray-600">© {new Date().getFullYear()} JollyGiggle. All rights reserved. 📝</p>
       </footer>
     </div>
-
   );
 }
